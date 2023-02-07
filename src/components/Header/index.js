085dad7bc8b68b614960499bb9db4bdccfefcad2
Argent <jrargent42@gmail.com>
2023-02-07 11:46:19 -0500

    update Header with bootstrap, comment out Header Box css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import Nav from "../Nav";
+import React from 'react';
+import Nav from '../Nav';
 
 function Header(props) {
   const { sections = [], setCurrentSection, currentSection } = props;
 
   return (
-    <header>
-      <div className="Header-Box">
+    <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <div className="d-flex align-items-center mb-3 mb-md-0 me-md-auto">
         <h1>
           <a href="/">Argent Portfolio</a>
         </h1>
