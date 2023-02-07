9771f4cd0700b833793535dcdb937882f608e87d
Argent <jrargent42@gmail.com>
2023-02-07 12:40:49 -0500

    update About with Bootstrap

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import headshot from '../../assets/images/Headshot.png';
-import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
 function About({ currentSection }) {
   const { name } = currentSection;
@@ -8,29 +8,42 @@ function About({ currentSection }) {
   if (name === 'About Me') {
     return (
       <section>
-        <h1 id="about">About Me</h1>
-        <div className="row-content">
-          <img
-            src={headshot}
-            alt="headshot of Argent in medieval garb, spinning yarn from a distaff"
-            className="about-me"
-          />
-          <p>
-            I come to the coding world from a bit of a different background. I
-            am an ancient and medieval textiles enthusiast and a member of the
-            Society for Creative Anachronism, where I interpret and bring to
-            life the history of spinning, weaving, and natural dyeing. The
-            detail-oriented nature of textiles translates well to the modern
-            craft of coding.
-            <br />
-            <Button variant="outline-success">Close me y'all!</Button>
-            <br />
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Repellendus, aliquid officia laborum, sapiente impedit consectetur
-            veritatis dolore aspernatur sed expedita quod reiciendis praesentium
-            quos adipisci corrupti odit possimus? Eius, deleniti?
-          </p>
-        </div>
+        <Container fluid>
+          <h1 className="justify-content-center display-6 text-center">
+            About Me
+          </h1>
+        </Container>
+
+        <Container fluid>
+          <div className=" row justify-content-center">
+            <div className="col col-10 row-content">
+              <img
+                src={headshot}
+                alt="headshot of Argent in medieval garb, spinning yarn from a distaff"
+                className="img-fluid rounded mx-auto d-block"
+              />
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col col-10 mt-4">
+              <p>
+                I come to the coding world from a bit of a different background.
+                I am an ancient and medieval textiles enthusiast and a member of
+                the Society for Creative Anachronism, where I interpret and
+                bring to life the history of spinning, weaving, and natural
+                dyeing. The detail-oriented nature of textiles translates well
+                to the modern craft of coding.
+                <br />
+                <br />
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Repellendus, aliquid officia laborum, sapiente impedit
+                consectetur veritatis dolore aspernatur sed expedita quod
+                reiciendis praesentium quos adipisci corrupti odit possimus?
+                Eius, deleniti?
+              </p>
+            </div>
+          </div>
+        </Container>
       </section>
     );
   }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 // import icons for github, linked in, and uhhhh
 
 function Footer() {
@@ -6,6 +7,7 @@ function Footer() {
     <footer>
       <div>
         <h3 className="fun">Argent Made This</h3>
+        <Button variant="outline-success">Click me y'all!</Button>
 
         <h4 className="hide-me">
           Fun fact: The first code was punch cards for Jacquard weaving looms.
