816f1e0defa9b3c2d6734d64b7e63b6c9058e720
Argent <jrargent42@gmail.com>
2023-02-14 16:14:50 -0500

    update footer to have github and linkedin links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 // import icons for github, linked in, and uhhhh
 
 function Footer() {
+  const useToggle = (initialState) => {
+    const [toggleValue, setToggleValue] = useState(initialState);
+
+    const toggler = () => {
+      setToggleValue(!toggleValue);
+    };
+    return [toggleValue, toggler];
+  };
+
+  const [toggle, setToggle] = useToggle();
+
   return (
     <footer>
-      <div>
-        <h3 className="fun">Argent Made This</h3>
-        <Button variant="outline-success">Click me y'all!</Button>
+      <div className="justify-content-center">
+        <h4 className="fun">Argent Made This</h4>
+        <div className="btn-group">
+          <a
+            type="button"
+            class="btn btn-outline-secondary"
+            href="https://github.com/jrargent/"
+            target="blank"
+          >
+            Please checkout my GitHub <FontAwesomeIcon icon={faGithub} />
+          </a>
 
-        <h4 className="hide-me">
-          Fun fact: The first code was punch cards for Jacquard weaving looms.
-          Coding and textiles go hand-in-hand!
-        </h4>
+          <a
+            type="button"
+            class="btn btn-outline-secondary"
+            href="https://www.linkedin.com/in/jennifer-r-argent/"
+            target="blank"
+          >
+            Or checkout my LinkedIn <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          {/* <Button variant="outline-success" onClick={setToggle}>
+          Click me y'all!
+        </Button> */}
+        </div>
+        {/* {toggle && (
+          <h4 className="hide-me">
+            Fun fact: The first code was punch cards for Jacquard weaving looms.
+            Coding and textiles go hand-in-hand!
+          </h4>
+        )} */}
       </div>
     </footer>
   );
